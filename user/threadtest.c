703bf42041a1f7610100abce28224f27ff9e34d8
yegi.mohamadi.8@gmail.com <yegi.mohamadi.8@gmail.com>
2025-07-05 13:45:14 +0330

    Fix thread output synchronization using user-level spinlock

--- user/threadtest.c
+++ user/threadtest.c
@@ -1,34 +1,57 @@
 #include "../kernel/types.h"
 #include "../user/user.h"
 
+#define STACK_SIZE 1024
 
-#define STACK_SIZE 100
+// قفل ساده user-space با استفاده از atomic operations
+typedef struct {
+  volatile int locked;
+} lock_t;
 
-void *my_thread(void *arg) {
-  uint64 number = (uint64) arg;
-  for (int i = 0; i < 100; ++i) {
-    number++;
-    printf("thread: %lu\n", number);
+void initlock(lock_t *lk) {
+  lk->locked = 0;
+}
+
+void acquire(lock_t *lk) {
+  while (__sync_lock_test_and_set(&lk->locked, 1) != 0)
+    ; // spinlock
+}
+
+void release(lock_t *lk) {
+  __sync_lock_release(&lk->locked);
+}
+
+lock_t printlock;
+
+// تابعی که توسط هر ترد اجرا می‌شود
+void* my_thread(void* arg) {
+  int id = (int)(uint64)arg;
+  for (int i = 0; i < 5; i++) {
+    acquire(&printlock);
+    printf("thread %d: iteration %d\n", id, i);
+    release(&printlock);
   }
-  return (void *) number;
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
-  int sp1[STACK_SIZE], sp2[STACK_SIZE], sp3[STACK_SIZE];
+  initlock(&printlock);
 
-  int ta = thread(my_thread, sp1 + STACK_SIZE, (void *) 100);
-  printf("NEW THREAD CREATED %d\n", ta);
+  // تعریف استک‌ها
+  static int stack1[STACK_SIZE];
+  static int stack2[STACK_SIZE];
+  static int stack3[STACK_SIZE];
 
-  int tb = thread(my_thread, sp2 + STACK_SIZE, (void *) 200);
-  printf("NEW THREAD CREATED %d\n", tb);
+  // ایجاد تردها
+  int tid1 = thread(my_thread, stack1 + STACK_SIZE, (void *)1);
+  int tid2 = thread(my_thread, stack2 + STACK_SIZE, (void *)2);
+  int tid3 = thread(my_thread, stack3 + STACK_SIZE, (void *)3);
 
-  int tc = thread(my_thread, sp3 + STACK_SIZE, (void *) 300);
-  printf("NEW THREAD CREATED %d\n", tc);
+  // منتظر ماندن برای اتمام تردها
+  jointhread(tid1);
+  jointhread(tid2);
+  jointhread(tid3);
 
-  jointhread(ta);
-  jointhread(tb);
-  jointhread(tc);
-
-  printf("DONE\n");
-  return 0;
+  printf("All threads finished.\n");
+  exit(0);
 }
